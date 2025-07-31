59ba44c81486cd55a71465e0ccda1366f736bca9
AnchalMish9630 <anchalmish2025@gmail.com>
2025-07-31 23:35:10 +0530

    basic config

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddTodo from './component/AddTodo';
+
+export const App = () => {
+  return (
+   <main>
+    <h1>
+      Todo List
+    </h1>
+    <AddTodo />
+   </main>
+  )
+}
+
+export default App;
