43766cb82b5d17ddda86dd8ced6c08d824659809
AnchalMish9630 <anchalmish2025@gmail.com>
2025-10-03 23:37:35 +0530

    changes  in sidebar

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export const Sidebar = () => {
+  // Dummy todo list (can be replaced with props or API data)
+  const todos = [
+    "Finish React assignment",
+    "Practice JavaScript DSA",
+    "Work on portfolio",
+    "Read Redux docs",
+    "Push code to GitHub"
+  ];
+
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="w-64 h-screen bg-gray-800 text-white p-4">
+      <h2 className="text-xl font-bold mb-4">📌 Todo Sidebar</h2>
+      <ul className="space-y-2">
+        {todos.map((todo, index) => (
+          <li
+            key={index}
+            className="bg-gray-700 p-2 rounded hover:bg-gray-600 cursor-pointer"
+          >
+            {todo}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
